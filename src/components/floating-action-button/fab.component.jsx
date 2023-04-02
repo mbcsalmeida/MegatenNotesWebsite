6ba7ec094900b3d5a0990bbe0ade1606468c7aed
mbcsalmeida <mbcsalmeida@gmail.com>
2023-04-02 17:54:44 +0100

    lil updates

--- src/components/floating-action-button/fab.component.jsx
+++ src/components/floating-action-button/fab.component.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import { FloatingActionBar, FloatingActionBarBlock, FloatingActionBarIcon, FloatingActionBarText } from './fab.styles';
-import Fab from '@mui/material/Fab ';
+import {Fab} from '@mui/material';
 
 export default class FloatingActionButton extends Component {
 
@@ -8,19 +8,29 @@ export default class FloatingActionButton extends Component {
         super()
 
         this.state = {
+            showOptions: false,
             showImg: false
         }
+        this.toggleFab = this.toggleFab.bind(this)
+    }
 
+    toggleFab(e){
+      this.setState(prevState => ({
+      showOptions: !prevState.showOptions
+      }));
     }
 
     render(){
         return(
-            <Fab color="primary" aria-label="add">
-                <FloatingActionBarBlock>
+            <Fab color="primary" aria-label="add" onClick={this.toggleFab}>
+                {this.state.showOptions &&
+                  <FloatingActionBarBlock>
                     <FloatingActionBarText>{this.showImg ? "ShowImages" : "Hide Images"}</FloatingActionBarText>
                     <FloatingActionBarText>Anchors</FloatingActionBarText>
-                </FloatingActionBarBlock>
-                <FloatingActionBarIcon />
+                  </FloatingActionBarBlock>
+                }
+                
+                 <FloatingActionBarIcon />
             </Fab>
         );
     }
